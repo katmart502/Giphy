ac821412ea20739dd404b21abb385900ce8ed74b
katherine martin <katherinemartin502@gmail.com>
2017-09-02 01:32:21 -0400

    Add files via upload

--- Gify.js
+++ Gify.js
@@ -1,72 +1,75 @@
  // Initial array of strings that will be assigned to buttons
-    var Michael Jackson = $(this).attr ("data-MichaelJackson");
+          var topic = [   
+     "moonwalk", "The Jacksons", "Jackson Five ", "King of Pop", "Jackson Family", "Thriller",
+    "Bad", "The Wiz", "Off The Wall", "Motown 25", "This is It",
+    "Smooth criminal", "Remember The Time", "", "", "",
+    "moonwalker", "Jackson brothers", "Dancing Machine", "I want you back", "ABC"
+];
 
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
-        topic+ "&api_key=dc6zaTOxFJmzC&limit=10";
-      
-  // Performing an AJAX request with the queryURL
-      $.ajax({
-          url: queryURL,
-          method: "GET"
-        })
-        // After data comes back from the request
-        .done(function(response) {
-          console.log(queryURL);
-          console.log(response);
-          // storing the data from the AJAX request in the results variable
-          var results = response.data;
-          // Looping through each result item
-          for (var i = 0; i < results.length; i++) {
-            // Creating and storing a div tag
-            var MichaelJacksonDiv = $("<div>");
-            // Creating a paragraph tag with the result item's rating
-            var p = $("<p>").text("Rating: " + results[i].rating);
-            // Creating and storing an image tag
-            var MichaelJacksonimage = $("<img>");
-            // Setting the src attribute of the image to a property pulled off the result item
-            MichaelJacksonImage.attr("src", results[i].images.fixed_height.url);
-            // Appending the paragraph and image tag to the animalDiv
-            MichaelJacksonDiv.append(p);
-            MichaelJacksonDiv.append(MichaelJacksonImage);
-            // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
-            $("#gifs-display-here").prepend(MichaelJacksonDiv);
-          }
-        });
-    };
-
-// Function for displaying data
-      function renderButtons() {
-        // Deleting the buttons prior to adding new movie buttons
-        // (this is necessary otherwise we will have repeat buttons)
-        $("#Jackson-view").empty();
-        // Looping through the array of movies
-        for (var i = 0; i < topic.length; i++) {
-          // Then dynamicaly generating buttons for each movie in the array.
-          // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
-          var a = $("<button>");
-          // Adding a class
-          a.addClass("Jackson");
-          // Adding a data-attribute with a value of the movie at index i
-          a.attr("data-name", topic[i]);
-          // Providing the button's text with a value of the movie at index i
-          a.text(topic[i]);
-          // Adding the button to the HTML
-          $("#Jackson-view").append(a);
-        }
+  // function to make buttons and add to page
+  function populateButtons(arrayToUse, classToAdd, areaToAddTo) {
+    $(areaToAddTo).empty();
+    for (var i = 0; i < arrayToUse.length; i++) {
+      var a = $("<button>");
+      a.addClass(classToAdd);
+      a.attr("data-type", arrayToUse[i]);
+      a.text(arrayToUse[i]);
+      $(areaToAddTo).append(a);
+    }
+  }
+  $(document).on("click", ".animal-button", function() {
+    $("#animals").empty();
+    $(".animal-button").removeClass("active");
+    $(this).addClass("active");
+    var type = $(this).attr("data-type");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=dc6zaTOxFJmzC&limit=10";
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+  
+.done(function(response) {
+      var results = response.data;
+      for (var i = 0; i < results.length; i++) {
+        var animalDiv = $("<div class=\"Jackson-item\">");
+        var rating = results[i].rating;
+        var p = $("<p>").text("Rating: " + rating);
+        var animated = results[i].images.fixed_height.url;
+        var still = results[i].images.fixed_height_still.url;
+        var animalImage = $("<img>");
+        MichaelJacksonImage.attr("src", still);
+        MichaelJacksonImage.attr("data-still", still);
+        MichaelJacksonImage.attr("data-animate", animated);
+        MichaelJacksonImage.attr("data-state", "still");
+        MichaelJacksonImage.addClass("Jackson-image");
+        MichaelJacksonDiv.append(p);
+        MichaelJacksonDiv.append(animalImage);
+        $("#Jackson").append(Micahel JacksonDiv);
       }
-      // This function handles events where one button is clicked
-      $("#add-Jackson").on("click", function(event) {
-        // event.preventDefault() prevents the form from trying to submit itself.
-        // We're using a form so that the user can hit enter instead of clicking the button if they want
-        event.preventDefault();
-        // This line will grab the text from the input box
-        var topic = $("#Jackson-input").val().trim();
-        // The movie from the textbox is then added to our array
-        topic.push(movie);
-        // calling renderButtons which handles the processing of our movie array
-        renderButtons();
-      });
-      // Calling the renderButtons function at least once to display the initial list of movies
-      renderButtons();
+    });
+  });
+  $(document).on("click", ".Jackson-image", function() {
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    }
+    else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  });
+  $("#add-Jackson").on("click", function(event) {
+    event.preventDefault();
+    var newMichaelJackson = $("input").eq(0).val();
+    if (newMichaelJackson.length > 2) {
+      Jacksons.push(newMichaelJackson);
+    }
+    populateButtons(Jacksons, "Jackson-button", "#Jackson-buttons");
+  });
+  populateButtons(animals, "Jackson-button", "#Jackson-buttons");
+});
 
-      };
+    
+
+    
